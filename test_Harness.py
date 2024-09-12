57992e1836eee45f7f4df8a09404798c4a7a83ca
AkeemSemper <55889346+AkeemSemper@users.noreply.github.com>
2024-09-12 14:28:52 -0600

    Update test_Harness.py

--- test_Harness.py
+++ test_Harness.py
@@ -44,7 +44,7 @@ def test_lab1Question2_4():
 def test_lab1Question2_5():
     # Test case 5
     name = ""
-    assert lab1Question2(name) == False
+    assert lab1Question2(name) == None
 
 def test_lab1Question3_1():
     # Test case 1
